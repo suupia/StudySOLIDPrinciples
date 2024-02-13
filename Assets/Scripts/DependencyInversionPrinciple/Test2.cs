5ac23f60f6b29c23ba2307b5197af55fe5cb2146
suupia <morita777k@icloud.com>
2024-02-13 20:47:57 +0900

    20240213

---
+++ Assets/Scripts/DependencyInversionPrinciple/Test2.cs
@@ -0,0 +1,47 @@
+﻿using LiskovsSubstitutionPrinciple;
+using UnityEngine.Assertions;
+
+namespace DependencyInversionPrinciple
+{
+    public class Test2
+    {
+        public void SlimeAttackTest()
+        {
+            // Arrange
+            var stabHpBar = new StabHPBar();
+            var hero = new Hero1(new StabHPBar(), 100);
+            var slime = new Slime(10);
+            var beforeHp = hero.Hp;
+            
+            // Act
+            slime.Attack(hero);
+            var afterHp = hero.Hp;
+            
+            // Assert
+            Assert.AreEqual(beforeHp- 10, afterHp); 
+        }
+        class StabHPBar : IUpdateHp
+        {
+            public void UpdateHp(int hp)
+            {
+                // 処理なし
+            }
+        }
+    }
+    
+
+
+    public class Slime
+    {
+        readonly int _atk;
+
+        public Slime(int atk)
+        {
+            _atk = atk;
+        }
+        public void Attack(Hero1 hero)
+        {
+            hero.SetDamage(_atk);
+        }
+    }
+}
